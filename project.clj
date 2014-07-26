613d7ed59874fa40a27fe2cc0390a92e5da27b11
Tzach Livyatan <tzach@cloudius-systems.com>
2014-07-26 11:21:49 +0300

    update project file to match clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tzach/capstan "0.1.0"
+(defproject capstan/lein-template "0.1.0"
   :description "generate a Capstan clojure project skeleton"
   :url "https://github.com/tzach/capstan-lein-plugin"
   :license {:name "3-clause BSD license"
