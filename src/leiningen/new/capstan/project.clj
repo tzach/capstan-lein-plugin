b27c601be89e57e77108504b76318b8361ed5afe
jwhitlark <jason@whitlark.org>
2014-10-07 14:22:39 -0700

    Bump template to latest clojure version.

--- src/leiningen/new/capstan/project.clj
+++ src/leiningen/new/capstan/project.clj
@@ -1,6 +1,6 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "Fix Me"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main  {{name}}.core
   :aot [{{name}}.core]
   :uberjar-name "{{name}}-standalone.jar"
